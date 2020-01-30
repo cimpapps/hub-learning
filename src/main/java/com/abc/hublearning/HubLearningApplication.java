8bb3906e59a97d7d8be90a2ce7f5d6974e07e368
catalincimpoeru <catalincimpoeru24@gmail.com>
2020-01-30 22:07:42 +0200

    Initial commit

---
+++ src/main/java/com/abc/hublearning/HubLearningApplication.java
@@ -0,0 +1,13 @@
+package com.abc.hublearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abc/hublearning/HubLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.hublearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
